e46e61861904c973f0e725df7ecd924eac479917
ytcctl <87226169+ytcctl@users.noreply.github.com>
2025-09-26 14:56:49 +0000

    Add FITE7410 Assignment

---
+++ FITE7410/FITE7410_Assignment_1_2025.R
@@ -0,0 +1,9 @@
+---
+title: "FITE7410 Assignment 1 - Exploratory Data Analysis"
+Student ID: "3036384024"
+date: "2025-10-01"
+output: html_document
+---
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
